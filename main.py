f1afd06b6c74d428f8cead8bd2d47d4c64483a16
Morgan Freeman <msmannan00@gmail.com>
2020-02-15 16:09:11 +0500

    bug Fixes
    bug Fixes

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 # READ COMMENTS
-file_data = pd.read_csv("training_data.csv")
-test_data = pd.read_csv("testing_data_1.csv")
+train_data = pd.read_csv("training_data.csv")
+test_data = pd.read_csv("input_data.csv")
 
-#CLEANING
-file_data['COMMENTS'] = file_data['COMMENTS'].map(lambda x: re.sub(r'[^A-Za-z ]+', '', x))
+# CLEANING
+train_data['COMMENTS'] = train_data['COMMENTS'].map(lambda x: re.sub(r'[^A-Za-z ]+', '', x))
 test_data['COMMENTS'] = test_data['COMMENTS'].map(lambda x: re.sub(r'[^A-Za-z ]+', '', x))
 
 # CREATE VECTORIZER
-count_vectorizer = CountVectorizer(analyzer='word',ngram_range=(1, 1),stop_words=None, token_pattern = '[a-zA-Z]+')
-transformed_vector = count_vectorizer.fit_transform(file_data["COMMENTS"].values.astype('U'))
-dataframe = pd.DataFrame(transformed_vector.toarray(),columns=count_vectorizer.get_feature_names())
+count_vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 1), stop_words=None, token_pattern='[a-zA-Z]+')
+transformed_vector = count_vectorizer.fit_transform(train_data["COMMENTS"].values.astype('U'))
+dataframe = pd.DataFrame(transformed_vector.toarray(), columns=count_vectorizer.get_feature_names())
 
 # SELECT KBEST
 selector = SelectKBest(score_func=chi2, k=3000)
-selector.fit(dataframe, file_data["PREDICTION"])
+selector.fit(dataframe, train_data["PREDICTION"])
 extracted_data = selector.transform(dataframe)
 extracted_feature = np.asarray(count_vectorizer.get_feature_names())[selector.get_support()]
 dataframe = dataframe[extracted_feature]
@@ -34,8 +34,8 @@ tokens_list = dataframe.columns.values
 
 # SPLIT TEST TRAIN
 data = dataframe[tokens_list[0:len(tokens_list)]]
-label = file_data["PREDICTION"]
-train_features, test_features, train_labels, test_labels = train_test_split(data, label, test_size = 0.3,shuffle=False)
+label = train_data["PREDICTION"]
+train_features, test_features, train_labels, test_labels = train_test_split(data, label, test_size=0.3, shuffle=False)
 
 # CREATE MODEL
 model = RandomForestClassifier(max_depth=150, random_state=10)
@@ -48,25 +48,24 @@ predictions = trainedModel.predict(test_features)
 
 # SHOW PREDICTIONS
 print(sklearn.metrics.accuracy_score(test_labels, predictions))
-print('F1 Score - ', {f1_score(test_labels, predictions,average='macro')})
+print('F1 Score - ', {f1_score(test_labels, predictions, average='macro')})
 print(precision_score(test_labels, predictions, average='macro'))
 print(recall_score(test_labels, predictions, average='macro'))
 
 # SHOW ACCURACY
 print(accuracy_score(test_labels, predictions))
 
-# PREPROCESS INPUT DATA
+# PREPROCESS OF INPUT
 transformed_vector = count_vectorizer.transform(test_data["COMMENTS"].values.astype('U'))
 transformed_dataframe = pd.DataFrame(transformed_vector.toarray(), columns=count_vectorizer.get_feature_names())
 transformed_dataframe = transformed_dataframe[extracted_feature]
 
-# TESTING
-print(dataframe)
+# TESTING OF INPUT
 transformed_dataframe.to_csv("./feature_matrix", encoding='utf-8', index=False)
 
-# PREDICTION
+# PREDICTION OF INPUT
 predictions = model.predict(transformed_dataframe)
 
-print("FINAL ACCURACY")
+# ACCURACY OF INPUT
 print(accuracy_score(test_data["PREDICTION"], predictions))
-np.savetxt("predictions.csv", predictions, delimiter=",",fmt="%s")
+np.savetxt("predictions.csv", predictions, delimiter=",", fmt="%s")
